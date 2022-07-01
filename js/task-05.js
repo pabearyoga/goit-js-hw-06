510895c36c5bd1ee7164de97043f19ce743503dc
Pabearyoga <pabearyoga@gmail.com>
2022-07-01 19:15:17 +0300

    fix task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,11 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
+const anon = "Anonimus";
 
 function onInputElInput() {
-  outputEl.textContent = inputEl.value;
+  inputEl.value === ""
+    ? (outputEl.textContent = anon)
+    : (outputEl.textContent = inputEl.value);
 }
 
 inputEl.addEventListener("input", onInputElInput);
